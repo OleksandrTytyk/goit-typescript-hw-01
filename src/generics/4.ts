d44d296e9801f2c9109e2acac6ad1fbc3f161381
Oleksandr Tytyk <aleks.tit3@gmail.com>
2024-04-27 15:45:49 +0300

    Add generics

---
+++ src/generics/4.ts
@@ -0,0 +1,13 @@
+type newUser = {
+	name: string;
+	surname: string;
+	email: string;
+	password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<newUser>) {}
+
+createOrUpdateUser({
+	email: "user@mail.com",
+	password: "password123",
+});
